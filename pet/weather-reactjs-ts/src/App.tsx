cfbdbe7fb217cb7be91da087bc393ec2af7849cf
tinhpv <vutinh.phan@gmail.com>
2023-07-05 01:08:49 +0700

    🧑🏻‍💻

--- pet/weather-reactjs-ts/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <main className="flex justify-center items-center bg-gradient-to-br from-cyan-300 via-teal-500 to-teal-700 h-[100vh] w-full">
-      <h1 className="text-3xl font-bold underline">Weather Aaron</h1>
-    </main>
-  );
-}
-
-export default App;
